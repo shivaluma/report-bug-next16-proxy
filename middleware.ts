b6ff7f64dd711d2f68f00f436ed1325593a09b18
shivaluma <shivaluma@gmail.com>
2025-11-14 16:48:15 +0700

    Add middleware for guest cookie and update Next.js configuration

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  // Set cookie isGuest = true
+  response.cookies.set("isGuest", "true", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+    path: "/",
+  });
+  return response;
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|public).*)"],
+};
